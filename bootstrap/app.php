7dd18ee08f9e11a1f3827953bfd93c0cfe4f6c5a
TonyUA2023 <tomyulloa123@gmail.com>
2026-09-07 17:38:17 -0500

    Solucionar Mixed Content y forzar HTTPS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->append(\App\Http\Middleware\HandleCustomDomain::class);
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
