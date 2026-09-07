280afab352e79d2f8cf6db32c05981f915174c56
TonyUA2023 <tomyulloa123@gmail.com>
2026-09-07 17:23:57 -0500

    Arreglar permisos de storage

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -ex
 
+echo "Configuring permissions..."
+chown -R www-data:www-data /var/www/html/storage
+chown -R www-data:www-data /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/storage
+chmod -R 775 /var/www/html/bootstrap/cache
+
 echo "Starting container setup..."
 php artisan config:clear || true
 
