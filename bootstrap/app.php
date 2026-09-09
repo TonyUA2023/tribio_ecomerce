0bafa87a1af1e7664ecaf6a91260c15888acb925
TonyUA2023 <tomyulloa123@gmail.com>
2026-09-08 23:14:12 -0500

    soluciones para ecomerce seleccion de pais

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
         ]);
+        $middleware->encryptCookies(except: [
+            'user_country',
+        ]);
         $middleware->validateCsrfTokens(except: [
             '/plan/webhook',
         ]);
